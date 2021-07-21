0c12644a99e1738901b227f1d5f496f877451e03
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-20 23:27:17 -0400

    Incorporación del counter

--- clase2react/src/App.tsx
+++ clase2react/src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  console.log(counter);
+
   return (
     <div className="App">
-
+      Counter = {counter}
+      <button onClick={(e) => setCounter(counter + 1)}>add 1</button>
     </div>
   );
 }
