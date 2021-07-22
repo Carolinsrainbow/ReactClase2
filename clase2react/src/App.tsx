62ef29e2ecc2b56093f175ce9fd3e73c01a1c61e
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-22 12:22:03 -0400

    Update App.tsx

--- clase2react/src/App.tsx
+++ clase2react/src/App.tsx
@@ -1,22 +1,29 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import useTitle from "./hooks/useTitle"
+import useTitle from "./hooks/useTitle";
 
 function App() {
   const [counter, setCounter] = useState(0);
   console.log(counter);
+  const [name, setName] = useState("");
 
-useEffect(()=>{
-  console.log("hay un cambio en el state")
-},[])
+  useEffect(() => {
+    console.log("hay un cambio en el state");
+  }, []);
 
-useTitle(counter + "")
+  useTitle(counter + "");
 
   return (
     <div className="App">
       Counter = {counter}
       <button onClick={() => setCounter(counter + 1)}>Sumar</button>
       <button onClick={() => setCounter(counter - 1)}>Restar</button>
+      <input
+        id="name"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
     </div>
   );
 }
